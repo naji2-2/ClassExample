57cc90093900630853ce1212a940af62e18fba73
Najisu <jisusunny0225@gmail.com>
2025-06-13 10:10:55 +0900

    상속 관계에서의 클래스 변수 예

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace ClassExample
 {
@@ -10,13 +6,25 @@ namespace ClassExample
     {
         class Parent
         {
+            public static int counter = 0;
+
+            public void CountParent()
+            {
+                Parent.counter++;
+            }
+
             public Parent() { Console.WriteLine("Parent()"); }
             public Parent(string name) { Console.WriteLine("Parent(string name)"); }
             public Parent(double param) { Console.WriteLine("Parent(double param)"); }
         }
 
-        class Child : Parent
-        {
+        class Child : Parent{
+
+            public void CountChild()
+            {
+                Child.counter++;
+            }
+
             public Child() : base("child")
             {
                 Console.WriteLine("Child()");
@@ -37,6 +45,16 @@ namespace ClassExample
         {
             Child childA = new Child("abc");
             Child childB = new Child(3);
+
+            Parent parent = new Parent();
+            Child child = new Child();
+
+            parent.CountParent();
+            child.CountChild();
+
+            Console.WriteLine(Parent.counter);
+            Console.WriteLine(Child.counter);
+
         }
     }
 }
